31e4c7b454122dbf067d99f5e3cf5e45d4fda165
Shivam Varshney <94469594+Shivv9761@users.noreply.github.com>
2023-07-09 09:07:57 +0530

    Update Catalog.jsx

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,11 +1,4 @@
-// import React, { useEffect, useState } from 'react'
-// import Footer from '../components/common/Footer'
-// import { useParams } from 'react-router-dom'
-// import { apiConnector } from '../services/apiconnector';
-// import { categories } from '../services/apis';
-// import { getCatalogaPageData } from '../services/operations/pageAndComponentData';
-// import Course_Card from '../components/core/Catalog/Course_Card';
-// import CourseSlider from '../components/core/Catalog/CourseSlider';
+
 import React, { useEffect, useState } from 'react'
 import Footer from '../components/common/Footer'
 import { useParams } from 'react-router-dom'
@@ -69,62 +62,7 @@ const Catalog = () => {
       }
     
 
-//   return (
-//     <div className='text-white'>
 
-//         <div>
-//             <p>{`Home / Catalog /`}
-//             <span>
-//                 {catalogPageData?.data?.selectedCategory?.name}
-//             </span></p>
-//             <p> {catalogPageData?.data?.selectedCategory?.name} </p>
-//             <p> {catalogPageData?.data?.selectedCategory?.description}</p>
-//         </div>
-
-//         <div>
-//             {/* section1 */}
-//             <div>
-//             <div>Courses to get you started</div>
-//                 <div className=' flex gap-x-3'>
-//                     <p>Most Popular</p>
-//                     <p>New</p>
-//                 </div>
-//                 <div>
-//                     <CourseSlider Courses={catalogPageData?.data?.selectedCategory?.courses} />
-//                 </div>
-//             </div>  
-
-//             {/* section2 */}
-//             <div>
-//             <div>Top Courses in {catalogPageData?.data?.selectedCategory?.name}</div>
-//                 <div>
-//                     <CourseSlider Courses={catalogPageData?.data?.differentCategory?.courses}/>
-//                 </div>
-//             </div>
-
-//             {/* section3 */}
-//             <div>
-//                 <div>Frequently Bought</div>
-//                 <div className='py-8'>
-
-//                     <div className='grid grid-cols-1 lg:grid-cols-2'>
-
-//                         {
-//                             catalogPageData?.data?.mostSellingCourses?.slice(0,4)
-//                             .map((course, index) => (
-//                                 <Course_Card course={course} key={index} Height={"h-[400px]"}/>
-//                             ))
-//                         }
-
-//                     </div>
-
-//                 </div>
-//             </div>
-
-//         </div>
-//     <Footer />
-//     </div>
-//   )
 return (
     <>
       {/* Hero Section */}
