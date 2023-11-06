cea306281aa61ea391e7fa9d570a78d5af90de68
Shivam Varshney <94469594+Shivv9761@users.noreply.github.com>
2023-11-06 06:47:45 +0530

    Update Profile.js

--- Server/controllers/Profile.js
+++ Server/controllers/Profile.js
@@ -127,29 +127,7 @@ exports.updateDisplayPicture = async (req, res) => {
 };
   
 exports.getEnrolledCourses = async (req, res) => {
-    // try {
-    //   const userId = req.user.id
-    //   const userDetails = await User.findOne({
-    //     _id: userId,
-    //   })
-    //     .populate("courses")
-    //     .exec()
-    //   if (!userDetails) {
-    //     return res.status(400).json({
-    //       success: false,
-    //       message: `Could not find user with id: ${userDetails}`,
-    //     })
-    //   }
-    //   return res.status(200).json({
-    //     success: true,
-    //     data: userDetails.courses,
-    //   })
-    // } catch (error) {
-    //   return res.status(500).json({
-    //     success: false,
-    //     message: error.message,
-    //   })
-    // }
+   
     try {
       const userId = req.user.id
       let userDetails = await User.findOne({
@@ -226,12 +204,10 @@ exports.instructorDashboard = async (req, res) => {
       const totalStudentsEnrolled = course.studentsEnrolled.length
       const totalAmountGenerated = totalStudentsEnrolled * course.price
 
-      // Create a new object with the additional fields
       const courseDataWithStats = {
         _id: course._id,
         courseName: course.courseName,
         courseDescription: course.courseDescription,
-        // Include other course properties as needed
         totalStudentsEnrolled,
         totalAmountGenerated,
       }
