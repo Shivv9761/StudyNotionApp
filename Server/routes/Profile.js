00fd626b6f473824928f772a446acf5689ce0cd4
Shivam Varshney <94469594+Shivv9761@users.noreply.github.com>
2023-11-06 06:59:16 +0530

    Update Profile.js

--- Server/routes/Profile.js
+++ Server/routes/Profile.js
@@ -10,14 +10,10 @@ const {
   instructorDashboard
 } = require("../controllers/Profile")
 
-// ********************************************************************************************************
-//                                      Profile routes
-// ********************************************************************************************************
-// Delet User Account
+
 router.delete("/deleteProfile",auth, deleteAccount)
 router.put("/updateProfile", auth, updateProfile)
 router.get("/getUserDetails", auth, getAllUserDetails)
-// Get Enrolled Courses
 router.get("/getEnrolledCourses", auth, getEnrolledCourses)
 router.put("/updateDisplayPicture", auth, updateDisplayPicture)
 router.get("/instructorDashboard", auth, isInstructor, instructorDashboard)
